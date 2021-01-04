12dacec1fb639a36f5a1924c488dc90b5a9ce6f7
Arif Budiman <arifbudiman@gmail.com>
2021-01-04 15:01:16 -0800

    Refactor

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import asyncio
-import math
 import os
 from datetime import datetime, timedelta
 from random import choice
@@ -21,41 +20,33 @@ class MyClient(discord.Client):
 
     myUserName = ""
     petIsTired = False
-    maxFatigue = 511
 
     async def on_ready(self):
 
-        channel = client.get_channel(botChannelId)
         await client.change_presence(afk=True)
+        channel = client.get_channel(botChannelId)
         self.myUserName = client.user.name + "#" + client.user.discriminator
-        restPeriod = math.ceil(18.3431952663 * self.maxFatigue)
-        attempt = 0
 
-        while True:
-            os.system("clear")
-            try:
-                print(f"{self.pacificTime()} Attempted {attempt}x")
-                if self.petIsTired == False:
-                    attempt += 1
-                    await channel.send("t!tg train")
-                elif self.petIsTired == True:
-                    print("{} Pet is fatigued. Resting until {}".format(self.pacificTime(), self.pacificTime() + timedelta(0, restPeriod)))
-                    await asyncio.sleep(restPeriod)
-                    self.petIsTired = False
-                    await channel.send("t!tg clean")
-                    await asyncio.sleep(7)
-                    await channel.send("t!tg play")
-                    await asyncio.sleep(7)
-                    await channel.send("t!tg feed")
-                    await asyncio.sleep(7)
+        print("{} Cleaning.".format(self.pacificTime()))
+        await channel.send("t!tg clean")
+        await asyncio.sleep(7)
 
-            except Exception as e:
-                print("ERR: ", e)
-                raise
+        print("{} Playing.".format(self.pacificTime()))
+        await channel.send("t!tg play")
+        await asyncio.sleep(7)
 
-            interval = choice(range(6, 9))
-            print(f"Waiting for {interval} seconds..")
-            await asyncio.sleep(interval)
+        print("{} Feeding.".format(self.pacificTime()))
+        await channel.send("t!tg feed")
+        await asyncio.sleep(7)
+
+        print("{} Started training.".format(self.pacificTime()))
+
+        while self.petIsTired == False:
+            await channel.send("t!tg train")
+            await asyncio.sleep(choice(range(6, 9)))
+
+        print("{} Pet is fatigued. Stopping".format(self.pacificTime()))
+        await client.close()
 
     async def on_message(self, message):
 
@@ -74,12 +65,9 @@ class MyClient(discord.Client):
             if self.myUserName in message.content and "Interacting with Pet" in message.content:
                 # if there's embed
                 if len(message.embeds) > 0:
-                    # if embed title indicates a successful training
-                    if "Training Success!" in message.embeds[0].title:
-                        print(message.embeds[0].title)
                     # if embed title indicates a failed training
-                    elif "Training Failed!" in message.embeds[0].title:
-                        print(message.embeds[0].title)
+                    if "Training Failed!" in message.embeds[0].title:
+                        # print(message.embeds[0].title)
                         self.petIsTired = True
 
     @staticmethod
